268d51ed917256383e2a45e959872d89f4aaca99
Panos <panosynt@gmail.com>
2024-11-04 12:18:11 +0200

    value objects description, name

--- core/domain/src/main/java/com/hexagonal/architecture/server/core/domain/valueobjects/Description.java
+++ core/domain/src/main/java/com/hexagonal/architecture/server/core/domain/valueobjects/Description.java
@@ -1,4 +1,45 @@
 package com.hexagonal.architecture.server.core.domain.valueobjects;
 
+import java.util.Objects;
+
 public class Description extends ValueObject {
+
+    private String value;
+
+    public Description(String value) {
+        this.value = value;
+    }
+
+    public static Description valueOf(String value) {
+        return new Description(value);
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Description that = (Description) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+
+    @Override
+    public String toString() {
+        return "Description{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+
 }
--- core/domain/src/main/java/com/hexagonal/architecture/server/core/domain/valueobjects/Name.java
+++ core/domain/src/main/java/com/hexagonal/architecture/server/core/domain/valueobjects/Name.java
@@ -1,4 +1,60 @@
 package com.hexagonal.architecture.server.core.domain.valueobjects;
 
+import java.util.Objects;
+
 public class Name extends ValueObject {
+
+    private String firstName;
+    private String lastName;
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public static Name valueOf(final String firstName, final String lastName) {
+        return new Name(firstName, lastName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Name name = (Name) o;
+        return Objects.equals(firstName, name.firstName) && Objects.equals(lastName, name.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Name{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
 }
