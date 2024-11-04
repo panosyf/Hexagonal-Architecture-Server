b27616f39c93d090cab5c0c6f3b2b42a4ce62f08
Panos <panosynt@gmail.com>
2024-11-04 14:39:20 +0200

    value objects timestamp

--- core/domain/src/main/java/com/hexagonal/architecture/server/core/domain/valueobjects/Timestamp.java
+++ core/domain/src/main/java/com/hexagonal/architecture/server/core/domain/valueobjects/Timestamp.java
@@ -1,4 +1,54 @@
 package com.hexagonal.architecture.server.core.domain.valueobjects;
 
+import java.time.Instant;
+import java.util.Objects;
+
 public class Timestamp extends ValueObject {
+
+    private Instant time;
+
+    private Timestamp() {
+        this.time = Instant.now();
+    }
+
+    private Timestamp(Instant time) {
+        this.time = Instant.now();
+    }
+
+    public static Timestamp valueOf(Instant time) {
+        return new Timestamp(time);
+    }
+
+    public Instant getTime() {
+        return this.time;
+    }
+
+    public boolean isBefore(Timestamp timestamp) {
+        return this.time.isBefore(timestamp.getTime());
+    }
+
+    public boolean isAfter(Timestamp timestamp) {
+        return this.time.isAfter(timestamp.getTime());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Timestamp timestamp = (Timestamp) o;
+        return Objects.equals(time, timestamp.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(time);
+    }
+
+    @Override
+    public String toString() {
+        return "Timestamp{" +
+                "time=" + time +
+                '}';
+    }
+
 }
